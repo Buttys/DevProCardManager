cde154251b01fd96abdb59ad743202c3dfca3552
sidschingis <sijing@gmx.net>
2017-07-07 21:53:03 +0200

    Update LinkMarker.cs

--- Enums/LinkMarker.cs
+++ Enums/LinkMarker.cs
@@ -6,7 +6,7 @@
         Bottom = 2,
         BottomRight = 4,
         Left = 8,
-        Middle = 10, // Only used for displaying
+        Middle = 16, // Only used for displaying
         Right = 32,
         TopLeft = 64,
         Top = 128,
